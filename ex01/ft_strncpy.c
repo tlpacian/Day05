c25598116c248f6dc1fc7b35b3ad530ca488780c
root <root@DESKTOP-OB7DKIA.localdomain>
2020-07-17 06:11:07 +0200

    strncpy

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+char      ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	
+	i = 0;
+	while (i < n && src[i] ! = '\0' }
+        {
+	     src[i] = dest[i];
+	     i++;
+	} 
+	while (i < n)
+	{
+	   dest[i] = '\0';
+	   i++;
+	}
+	return (dest);
+}
